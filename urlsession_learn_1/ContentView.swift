09d9c24f0d6f62a07dcb1ec58dae6ec5da20442c
hadohado <hadohado@gmail.com>
2021-06-19 21:17:34 -0700

    Initial Commit

---
+++ urlsession_learn_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  urlsession_learn_1
+//
+//  Created by ha tuong do on 6/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ urlsession_learn_1Tests/urlsession_learn_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  urlsession_learn_1Tests.swift
+//  urlsession_learn_1Tests
+//
+//  Created by ha tuong do on 6/19/21.
+//
+
+import XCTest
+@testable import urlsession_learn_1
+
+class urlsession_learn_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
